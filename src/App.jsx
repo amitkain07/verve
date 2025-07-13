a461e4306fd044f4686f6df1568accfcd35ab63e
amitkain7 <kainturagolu688@gmail.com>
2025-07-13 07:20:15 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar'
 import Partners from './components/Partners'
 import Footer from './components/Footer'
 import Services from './components/Services'
-import Hero from './Components/Hero'
+import Hero from './components/Hero'
 import Destination from './components/Destination'
 import NewsLatter from './components/NewsLatter'
 import Testimonials from './components/Testimonials'
@@ -14,7 +14,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <Hero />
+      <Hero/>
       <Services />
       <Destination />
       <Trip />
